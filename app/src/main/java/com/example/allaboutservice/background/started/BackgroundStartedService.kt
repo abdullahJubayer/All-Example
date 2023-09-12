9a4ce87499bbee8616f58c71eeae58399181798c
abdullah jubayer <abdullahjubayer38@gmail.com>
2023-09-12 15:30:28 +0600

    Exploring START_NOT_STICKY

--- app/src/main/java/com/example/allaboutservice/background/started/BackgroundStartedService.kt
+++ app/src/main/java/com/example/allaboutservice/background/started/BackgroundStartedService.kt
@@ -9,19 +9,25 @@ import android.os.Looper
 import android.os.Message
 import android.os.Process
 import com.example.allaboutservice.show
+import java.lang.Exception
 
 
 class BackgroundStartedService : Service() {
 
     private lateinit var serviceHandler: ServiceHandler
+    private lateinit var backgroundThread: HandlerThread
 
     private inner class ServiceHandler(looper: Looper) : Handler(looper) {
         override fun handleMessage(msg: Message) {
             "HandleMessage : ${msg.arg1}".show()
 
-            "ServiceHandlerThread.Start".show()
-            Thread.sleep(50000)
-            "ServiceHandlerThread.End".show()
+            try {
+                "ServiceHandlerThread${msg.arg1}.Start".show()
+                Thread.sleep(400)
+                "ServiceHandlerThread${msg.arg1}.End".show()
+            }catch (_:Exception){
+                "ServiceHandlerThread.ForceStop".show()
+            }
 
         }
 
@@ -34,7 +40,7 @@ class BackgroundStartedService : Service() {
 
 
         /* Create Thread */
-        val backgroundThread = HandlerThread("MyService Thread", Process.THREAD_PRIORITY_BACKGROUND)
+        backgroundThread = HandlerThread("MyService Thread", Process.THREAD_PRIORITY_BACKGROUND)
         /* Start Thread */
         backgroundThread.start()
         /* Get Looper From Thread */
@@ -56,21 +62,21 @@ class BackgroundStartedService : Service() {
 
         /* We can use HandlerThread to handle our operation in background. and this execute like a queue when new Intent arrived */
         serviceHandler.obtainMessage().also {
-            it.arg1 = startId
+            it.arg1 = intent?.getIntExtra("Id",-1) ?: -1
             serviceHandler.sendMessage(it)
         }
 
 
         /* Or We can use Thread class to handle our operation in background. But this will execute as soon as new Intent arrived */
-        Thread(Runnable {
-            run {
-                "Thread2.Start".show()
-                Thread.sleep(50000)
-                "Thread2.End".show()
-            }
-        }).start()
+//        Thread(Runnable {
+//            run {
+//                "Thread2.Start".show()
+//                Thread.sleep(50000)
+//                "Thread2.End".show()
+//            }
+//        }).start()
 
-        return Service.START_STICKY
+        return Service.START_NOT_STICKY
     }
 
     /* Return Null Because No  Binding Needed */
@@ -80,9 +86,20 @@ class BackgroundStartedService : Service() {
     }
 
 
+    /* Very important that if we use handler in another thread. even the service is destroy still the handler will do its work. so we need to remove handler and stop the thread */
     override fun onDestroy() {
         "onDestroy".show()
+        destroyThreadAndHandler()
         super.onDestroy()
+    }
+
+    private fun  destroyThreadAndHandler(){
+        /* This is very important to remove callback from handler & stop the background thread*/
+        serviceHandler.removeCallbacksAndMessages(null)
+        if (backgroundThread.isAlive){
+            backgroundThread.quit() /* quite() will remove all pending work for this thread. but currently running work will not stop */
+            backgroundThread.interrupt() /* So interrupt() will stop current work and  throw an interrupt exception and we should handle this exception */
+        }
     }
 
 }
