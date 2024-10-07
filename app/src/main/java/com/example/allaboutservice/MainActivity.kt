d036981bfaf9be7d347b7de742ef1cb2556c5af7
abdullah <abdullahjubayer38@gamil.com>
2024-10-08 00:43:28 +0600

    working on custom views

--- app/src/main/java/com/example/allaboutservice/MainActivity.kt
+++ app/src/main/java/com/example/allaboutservice/MainActivity.kt
@@ -1,69 +1,16 @@
 package com.example.allaboutservice
 
-import android.app.PendingIntent
-import android.content.ComponentName
-import android.content.Context
-import android.content.Intent
-import android.content.ServiceConnection
 import android.os.Bundle
-import android.os.Handler
-import android.os.IBinder
-import android.os.Looper
 import androidx.appcompat.app.AppCompatActivity
-import com.example.allaboutservice.background.bound.StartedAndBoundService
-import com.example.allaboutservice.background.started.BackgroundStartedService
 
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var mService: StartedAndBoundService
-    private var mBound: Boolean = false
 
-
-    private val serviceConnection : ServiceConnection = object  : ServiceConnection{
-        override fun onServiceConnected(p0: ComponentName?, binder: IBinder?) {
-            mService = (binder as StartedAndBoundService.MyBinder).getStartedAndBoundService()
-            mBound=true
-        }
-
-        override fun onServiceDisconnected(p0: ComponentName?) {
-            mBound=false
-        }
-
-    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         "MainActivity-onCreate".show()
 
-        /* service before 40s. then call the pending intent to start the service again*/
-        Handler(Looper.getMainLooper()).postDelayed(Runnable {
-            run {
-                Intent(this@MainActivity,StartedAndBoundService::class.java).apply {
-                    bindService(this,serviceConnection,Context.BIND_AUTO_CREATE)
-                }
-            }
-        }, 40*1000)
-
-
-        Handler(Looper.getMainLooper()).postDelayed(Runnable {
-            run {
-                if (mBound){
-
-                }
-            }
-        }, 60*1000)
-
-    }
-
-
-    override fun onStart() {
-        startService(Intent(this, BackgroundStartedService::class.java))
-        super.onStart()
-    }
-
-    override fun onDestroy() {
-//        stopService(Intent(this, BackgroundStartedService::class.java))
-        super.onDestroy()
     }
 }
---
+++ app/src/main/java/com/example/allaboutservice/PathViews.kt
@@ -0,0 +1,43 @@
+package com.example.allaboutservice
+
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.graphics.Path
+import android.util.AttributeSet
+import android.view.View
+
+class PathViews  @JvmOverloads constructor(ctx:Context,attrSet: AttributeSet ? = null, defStyleAttr : Int = 0, defStyleRes:Int =0): View(ctx,attrSet,defStyleAttr,defStyleRes) {
+
+    private val paint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
+        color = Color.RED
+    }
+    private val paint2 = Paint(Paint.ANTI_ALIAS_FLAG).apply {
+        color = Color.RED
+        style = Paint.Style.STROKE
+    }
+
+    private val path = Path()
+
+    override fun onDraw(canvas: Canvas) {
+        super.onDraw(canvas)
+
+        drawFillX(canvas)
+        drawArc(canvas)
+    }
+
+
+    private  fun drawFillX(canvas: Canvas){
+        path.moveTo(0.1f * width, 0.1f * height)
+        path.lineTo(0.1f * width, 0.5f * height)
+        path.lineTo(0.9f * width, 0.1f * height)
+        path.lineTo(0.9f * width, 0.5f * height)
+        canvas.drawPath(path, paint)
+    }
+
+    private fun drawArc(canvas: Canvas){
+        canvas.drawArc(0.1f * width, 0.6f * height,0.9f * width,0.9f * height,180f,90f,true,paint)
+    }
+
+}
