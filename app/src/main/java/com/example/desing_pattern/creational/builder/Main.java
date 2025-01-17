e7b2dff2af34426086936e5c69aa268b6567e1fe
abdullah <abdullahjubayer38@gamil.com>
2025-01-17 21:52:40 +0600

    add builder pattern

--- app/src/main/java/com/example/desing_pattern/creational/builder/Main.java
+++ app/src/main/java/com/example/desing_pattern/creational/builder/Main.java
@@ -1,5 +1,14 @@
 package com.example.desing_pattern.creational.builder;
 
+
+    /*
+    -------------Factory Pattern-------------
+        The builder pattern used to construct complex objects step by step.
+        It allows the creation of an object with a large number of optional
+        and required parameters, often making the object construction process cleaner and more readable.
+    ------------Factory Pattern--------------
+    */
+
 public class Main {
     public static void main(String[] args) {
         Retrofit retrofit = new Retrofit.RetrofitBuilder()
